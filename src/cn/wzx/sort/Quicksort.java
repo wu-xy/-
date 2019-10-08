11dab5d0e1efeb9b525119961314644776461ea3
Administrator <Administrator@PC-20190530KLTS>
2019-10-08 23:15:02 +0800

    快速排序

--- src/cn/wzx/sort/Quicksort.java
+++ src/cn/wzx/sort/Quicksort.java
@@ -16,19 +16,19 @@ public class Quicksort {
 	public static void quicksort(int[] intarr,int start,int end) {
 		if(start<end) {
 			int standard=intarr[start];		//比较的标准数字
-			int low=start;
-			int high=end;
+			int low=start;		//左游标
+			int high=end;		//右游标
 			while(low<high) {
-				while(low<high&&intarr[high]>=standard) {
+				while(low<high&&intarr[high]>=standard) {	//如果右游标指向的数值比标准值大，右游标向左移
 					high--;
 				}
-				intarr[low]=intarr[high];
-				while(low<high&&intarr[low]<=standard) {
+				intarr[low]=intarr[high];	//此时右游标指向的数值比标准值小，将右游标的值赋给左游标，并开始移动左游标
+				while(low<high&&intarr[low]<=standard) {	//如果左游标指向的数值比标准值小，左游标向右移
 					low++;
 				}
 				intarr[high]=intarr[low];
 			}
-			intarr[low]=standard;
+			intarr[low]=standard;	//此时左右游标重合
 			quicksort(intarr, start, low);	//处理小的一边
 			quicksort(intarr, low+1, end);	//处理大的一边
 		}
